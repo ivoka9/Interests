c5fbdcba877989799bf8c0846745f57e9654549d
ivoka <ivailo96g@gmail.com>
2020-07-15 19:09:08 -0400

    heroku

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const url = "mongodb://127.0.0.1:27017/final";
 
 // Mongoose connection
 mongoose
-  .connect(process.env.MONGODB_URL || url, {
+  .connect(process.env.MONGODB_URI || url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
